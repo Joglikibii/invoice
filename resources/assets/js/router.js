24927955785fb607caa4110cdf6a112d8caaa841
Danny Avery <danny.w.avery@gmail.com>
2017-03-08 14:16:07 -0700

    Form.vue for editing customer

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
 	routes: [
 		{ path: '/', component: require('./views/customer/index.vue') },
+		{ path: '/customer/:id/edit', component: require('./views/customer/form.vue'), meta: { mode: 'edit' }},
 		{ path: '/customer/:id', component: require('./views/customer/show.vue') }
 	]
 })
